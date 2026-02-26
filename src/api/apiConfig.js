fd66c1889659db521b04c96f20721dafb157361c
abdfthsaid <abdifatahsaedmxm@gmail.com>
2026-02-26 15:31:45 +0300

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -73,7 +73,7 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
